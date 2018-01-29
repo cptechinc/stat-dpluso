901d31d500ff18ccf6a019ecd78944f563e9ce60
Paul Gomez <pauldro952@outlook.com>
2018-01-29 15:23:41 -0600

    Use whseid for id
    (cherry picked from commit cptechinc/soft-crm@08ae86dea14b2fe46d8939689b2a918f4ef93ff6)

--- vendor/cptechinc/dpluso-screen-formatters/src/ci/QuotesFormatter.class.php
+++ vendor/cptechinc/dpluso-screen-formatters/src/ci/QuotesFormatter.class.php
@@ -19,7 +19,7 @@
 			$content = '';
             
             foreach ($this->json['data'] as $whseid => $whse) {
-                $tb = new Table("class=table table-striped table-bordered table-condensed table-excel|id=$whse");
+                $tb = new Table("class=table table-striped table-bordered table-condensed table-excel|id=$whseid");
             	$tb->tablesection('thead');
             		for ($x = 1; $x < $this->tableblueprint['detail']['maxrows'] + 1; $x++) {
             			$tb->tr();

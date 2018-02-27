8adfe7cc2229b967137cc0f5f6f09389f2e7bef9
Paul Gomez <pauldro952@outlook.com>
2018-02-27 10:04:52 -0600

    Documentation of Contact class
    (cherry picked from commit cptechinc/soft-crm@152a3a0e3d62e640986a2744ea8d3b62c056115a)

--- vendor/cptechinc/dpluso-processwire-classes/src/Model/Contact.class.php
+++ vendor/cptechinc/dpluso-processwire-classes/src/Model/Contact.class.php
@@ -50,13 +50,18 @@
             }
         }
         
+		/**
+		 * Grabs the name of the customer off the contact object, and if blank, 
+		 * it will just return custid
+		 * @return string customername
+		 */
         public function get_customername() {
             return (!empty($this->name)) ? $this->name : $this->custid;
         }
         
         /**
          * Returns if Contact has a shiptoid
-         * @return boolean [description]
+         * @return bool [description]
          */
         public function has_shipto() {
             return (!empty($this->shiptoid));
@@ -64,7 +69,7 @@
         
         /**
          * Returns if contact has phone extension 
-         * @return boolean [description]
+         * @return bool [description]
          */
         protected function has_extension() {
             return ($this->cphext != '') ? true : false;
@@ -73,22 +78,27 @@
         /* =============================================================
 			CLASS FUNCTIONS 
 		============================================================ */
+		/**
+		 * Generates the URL to the customer page which currently
+		 * goes to load the CI Page.
+		 * @return string 
+		 */
         public function generate_customerurl() {
             return $this->generate_ciloadurl();
         }
-
+		
+		/**
+		 * Generates the customer URL but also defines the Shiptoid in the URL
+		 * @return string
+		 */
         public function generate_shiptourl() {
             return $this->generate_customerurl() . "&shipID=".urlencode($this->shiptoid);
         }
-
-		public function generatecustloadurl() {
-			if ($this->has_shipto()) {
-				return $this->generate_shiptourl();
-			} else {
-				return $this->generate_customerurl();
-			}
-		}
-
+		
+		/**
+		 * Generates URL to the contact page
+		 * @return string
+		 */
         public function generate_contacturl() {
             $url = new \Purl\Url(Processwire\wire('config')->pages->contact);
             $url->query->set('custID', $this->custid);
@@ -99,7 +109,11 @@
             $url->query->set('id', $this->contact);
             return $url->getUrl();
         }
-
+		
+		/** 
+		 * Generates the load customer URL to get to the CI PAGE
+		 * @return string
+		 */
 	    public function generate_ciloadurl() {
             $url = $this->generate_redirurl();
             $url->query->set('action', 'ci-customer');
@@ -111,6 +125,11 @@
             return $url->getUrl();
 		}
         
+		/**
+		 * URL to redirect page to set the customer for the cart, 
+		 * redirects to the cart
+		 * @return string
+		 */
         public function generate_setcartcustomerurl() {
             $url = $this->generate_redirurl();
             $url->query->set('action', 'shop-as-customer');
@@ -122,14 +141,18 @@
             return $url->getUrl();
         }
         
+		/**
+		 * URL to the customer redirect page, will be used by other functions to extend on
+		 * @return [type] [description]
+		 */
         public function generate_redirurl() {
             return new \Purl\Url(Processwire\wire('config')->pages->customer."redir/");
         }
         
         /**
          * Outputs the javascript function name with parameter
-         * @param String $function which II function
-         * @return String          Function name with parameter for the call
+         * @param string $function which II function
+         * @return string          Function name with parameter for the call
          */
         function generate_iifunction($function) {
             switch ($function) {
@@ -144,7 +167,12 @@
                     break;
             }
         }
-
+		
+		/**
+		 * Returns Phone with extension 
+		 * or without it depending if it has one
+		 * @return string
+		 */
 		public function generate_phonedisplay() {
 			if ($this->has_extension()) {
 				return $this->cphone . ' &nbsp; ' . $this->cphext;
@@ -152,7 +180,12 @@
 				return $this->cphone;
 			}
 		}
-
+		
+		/**
+		 * Takes the method type and makes a proper URL depending on the method
+		 * @param  string $method two main groups : phone / email
+		 * @return string         url with with the protocol defined
+		 */
 		public function generate_contactmethodurl($method) {
 			switch ($method) {
 				case 'cell':
@@ -169,7 +202,11 @@
 					break;
 			}
 		}
-
+		
+		/**
+		 * Generates a one line address string
+		 * @return string
+		 */
 		public function generate_address() {
 			return $this->addr1 . ' ' . $this->addr2. ' ' . $this->ccity . ', ' . $this->cst . ' ' . $this->czip;
 		}
@@ -178,6 +215,13 @@
 			OTHER CONSTRUCTOR FUNCTIONS 
             Inherits some from CreateFromObjectArrayTraits
 		============================================================ */
+		/**
+		 * Loads an object with this class using the parameters as provided
+		 * @param  string $custID    CustomerID
+		 * @param  string $shiptoID  Shipto ID (can be blank)
+		 * @param  string $contactID Contact ID (can be blank)
+		 * @return Contact
+		 */
         public static function load($custID, $shiptoID = '', $contactID = '') {
             return get_customercontact($custID, $shiptoID, $contactID);
         }
@@ -188,6 +232,14 @@
 			public static function generate_classarray()
 			public function _toArray()
 		============================================================ */
+		/**
+		 * Mainly called by the _toArray() function which makes an array
+		 * based of the properties of the class, but this function filters the array
+		 * to remove keys that are not in the database
+		 * This is used by database classes for update
+		 * @param  array $array array of the class properties
+		 * @return array]        with certain keys removed
+		 */
  		public static function remove_nondbkeys($array) {
 			unset($array['fieldaliases']);
  			return $array;

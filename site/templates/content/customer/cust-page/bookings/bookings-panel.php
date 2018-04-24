a5aa088acce38df5405c2f6209bc9834f92ecd55
Paul Gomez <pauldro952@outlook.com>
2018-04-24 13:08:45 -0500

    Fixed if data missing on sales data, and bookings
    (cherry picked from commit cptechinc/soft-crm@53ce51e52712f05dd5002003429af96a4450afcf)

--- site/templates/content/customer/cust-page/bookings/bookings-panel.php
+++ site/templates/content/customer/cust-page/bookings/bookings-panel.php
@@ -3,7 +3,7 @@
 	$bookingspanel->generate_filter($input);
     $bookingspanel->set_customer($customer->custid, $customer->shiptoid);
 	$bookings = $bookingspanel->get_bookings();
-	
+
 	foreach ($bookings as $booking) {
 		$bookdata = array(
 			'bookdate' => DplusDateTime::format_date($booking['bookdate'], 'Y-m-d'),
@@ -12,7 +12,7 @@
 		if ($bookingspanel->interval == 'day') {
 			$bookdata['dayurl'] = $bookingspanel->generate_viewsalesordersbydaylink($booking['bookdate']);
 		}
-		
+
 		$bookingdata[] = $bookdata;
 	}
 ?>
@@ -27,39 +27,45 @@
 			<span class="pull-right"><?= $bookingspanel->generate_todaysbookingsdescription(); ?></span>
 		</a>
 	</div>
-	<div id="bookings-div" class="" aria-expanded="true">
-		<div class="panel-body">
-			<button class="btn btn-primary toggle-order-search pull-right" type="button" data-toggle="collapse" data-target="#bookings-search-div" aria-expanded="false" aria-controls="bookings-search-div">Toggle Search <i class="fa fa-search" aria-hidden="true"></i></button>
-			<div id="bookings-search-div" class="<?= (!empty($bookingspanel->filters)) ? 'collapse' : ''; ?>">
-				<?php include $config->paths->content.'customer/cust-page/bookings/search-form.php'; ?>
+	<div id="bookings-div" aria-expanded="true">
+		<?php if (sizeof($bookings)) : ?>
+			<div class="panel-body">
+				<button class="btn btn-primary toggle-order-search pull-right" type="button" data-toggle="collapse" data-target="#bookings-search-div" aria-expanded="false" aria-controls="bookings-search-div">Toggle Search <i class="fa fa-search" aria-hidden="true"></i></button>
+				<div id="bookings-search-div" class="<?= (!empty($bookingspanel->filters)) ? 'collapse' : ''; ?>">
+					<?php include $config->paths->content.'customer/cust-page/bookings/search-form.php'; ?>
+				</div>
 			</div>
-		</div>
-		<div>
-			<h3 class="text-center"><?= $bookingspanel->generate_title(); ?></h3>
-			<div id="bookings-chart">
-				
-			</div>
-			<div class="table-responsive bookings-table-div">
-				<div class="row">
-					<div class="col-sm-6">
-						<div class="jumbotron item-detail-heading"> <div> <h4>Booking Dates</h4> </div> </div>
-						<div class="table-responsive">
-							<?php include $config->paths->content."customer/cust-page/bookings/$bookingspanel->interval-table.php"; ?>
+			<div>
+				<h3 class="text-center"><?= $bookingspanel->generate_title(); ?></h3>
+				<div id="bookings-chart">
+
+				</div>
+				<div class="table-responsive bookings-table-div">
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="jumbotron item-detail-heading"> <div> <h4>Booking Dates</h4> </div> </div>
+							<div class="table-responsive">
+								<?php include $config->paths->content."customer/cust-page/bookings/$bookingspanel->interval-table.php"; ?>
+							</div>
 						</div>
-					</div>
-					<div class="col-sm-6">
-						<div class="jumbotron item-detail-heading"> <div> <h4>Shipto Bookings</h4> </div> </div>
-						<div class="table-responsive">
-							<?php include $config->paths->content."customer/cust-page/bookings/shipto-booking-totals-table.php"; ?>
+						<div class="col-sm-6">
+							<div class="jumbotron item-detail-heading"> <div> <h4>Shipto Bookings</h4> </div> </div>
+							<div class="table-responsive">
+								<?php include $config->paths->content."customer/cust-page/bookings/shipto-booking-totals-table.php"; ?>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>	
-		</div>
+			</div>
+		<?php else : ?>
+			<div class="panel-body">
+				<p class="text-center">No <?= $customer->get_customername(); ?> Bookings found</p>
+			</div>
+		<?php endif; ?>
 	</div>
 </div>
-<?php 
+<?php
 	if (!empty($bookingdata)) {
-		include $config->paths->content."customer/cust-page/bookings/bookings-line-chart.js.php"; 
+		include $config->paths->content."customer/cust-page/bookings/bookings-line-chart.js.php";
 	}
 ?>

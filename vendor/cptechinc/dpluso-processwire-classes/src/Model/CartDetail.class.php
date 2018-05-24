4c4aea7d25a440175a0dbe364691be0eeee0abd7
Paul Gomez <pauldro952@outlook.com>
2018-05-24 10:20:03 -0500

    Merge pull request #169 from cptechinc/edit-detail2
    Shows can back order on the view detail
    
    (cherry picked from commit cptechinc/soft-dpluso@7a675b8a2b46a040f4a09ac5ebf7cf96a03ab3dd)

--- vendor/cptechinc/dpluso-processwire-classes/src/Model/CartDetail.class.php
+++ vendor/cptechinc/dpluso-processwire-classes/src/Model/CartDetail.class.php
@@ -1,26 +1,93 @@
-<?php 
+<?php
 	/**
 	 * Class for the Individual Items in the User's Cart
 	 */
 	class CartDetail extends OrderDetail implements OrderDetailInterface {
 		use CreateFromObjectArrayTraits;
 		use CreateClassArrayTraits;
-	
+
 		protected $orderno;
+
+		/**
+		 * Price of Item
+		 * @var float
+		 */
 		protected $price;
+
+		/**
+		 * Item Quantity
+		 * @var int
+		 */
 		protected $qty;
+
+		/**
+		 * Quantity Shipped
+		 * will be 0
+		 * @var int
+		 */
 		protected $qtyshipped;
+
+		/**
+		 * Quantity backordered
+		 * will be 0
+		 * @var int
+		 */
 		protected $qtybackord;
+
+		/**
+		 * If Item has Documents
+		 * @var string Y | N
+		 */
 		protected $hasdocuments;
+
+		/**
+		 * Quantity Available | In Stock
+		 * @var int
+		 */
 		protected $qtyavail;
+
+		/**
+		 * Item Cost
+		 * @var float
+		 */
 		protected $cost;
+
+		/**
+		 * Coupon or Promo code applied
+		 * @var string
+		 */
 		protected $promocode;
+
+		/**
+		 * Tax Code Percent
+		 * @var float
+		 */
 		protected $taxcodeperc;
+
+		/**
+		 * Unit of Measurement
+		 * @var string
+		 */
 		protected $uomconv;
+
+		/**
+		 * [protected description]
+		 * @var string
+		 */
 		protected $catlgid;
+
+		/**
+		 * Purchase Order Number for item
+		 * @var string
+		 */
 		protected $ponbr;
+
+		/**
+		 * Purchase Order Reference for item
+		 * @var string
+		 */
 		protected $poref;
-				
+
 		/* =============================================================
 			GETTER FUNCTIONS
 		============================================================ */
@@ -32,10 +99,6 @@
 		public function has_error() {
 			return !empty($this->errormsg);
 		}
-		
-		public function is_kititem() {
-			return $this->kitemflag == 'Y' ? true : false;
-		}
 		/**
 		 * Checks if detail has documents by looking at the document flag
 		 * @return bool $this->hasdocuments == 'Y'
@@ -43,15 +106,15 @@
 		public function has_documents() {
 			return $this->hasdocuments == 'Y' ? true : false;
 		}
-		
+
 		/**
-		 * Checks if there's dplus Qnotes 
+		 * Checks if there's dplus Qnotes
 		 * @return bool Calls to Database for Qnote Count for this line #
 		 */
 		public function has_notes() {
 			return has_dplusnote($this->sessionid, $this->sessionid, $this->linenbr, Processwire\wire('config')->dplusnotes['cart']['type']) == 'Y' ? true : false;
 		}
-		
+
 		/**
 		 * Returns if Cart Detail is editable AND should always be by default
 		 * @return bool Can the cart detail be editable
@@ -59,19 +122,37 @@
 		public function can_edit() {
 			return true;
 		}
-		
+
+		public function can_backorder() {
+			return 'N/A';
+		}
+
+		public function display_canbackorder() {
+			return '';
+		}
+
 		/* =============================================================
-			GENERATE DPLUS DATA FUNCTIONS 
+			GENERATE DPLUS DATA FUNCTIONS
 		============================================================ */
-			
-			function generate_editdetaildata($custID, $shipID = false) {
-				$data = array('DBNAME' => Processwire\wire('config')->dbName, 'CARTDET' => false, 'LINENO' => $this->linenbr);
-				$data['CUSTID'] = empty($custID) ? $config->defaultweb : $custID;
-				if (!empty($shipID)) {$data['SHIPTOID'] = $shipID; }
-				return $data;
-			}
+		/**
+		 * Generates the array for writing Data to Dplus
+		 * @param  string  $custID Customer ID
+		 * @param  mixed $shipID ShiptoID or false
+		 * @return array          [description]
+		 */
+		function generate_editdetaildata($custID, $shipID = false) {
+			$data = array(
+				'DBNAME' => DplusWire::wire('config')->dbName,
+				'CARTDET' => false,
+				'LINENO' => $this->linenbr
+			);
+			$data['CUSTID'] = empty($custID) ? $config->defaultweb : $custID;
+			if (!empty($shipID)) {$data['SHIPTOID'] = $shipID; }
+			return $data;
+		}
+
 		/* =============================================================
-			GENERATE ARRAY FUNCTIONS 
+			GENERATE ARRAY FUNCTIONS
 			The following are defined CreateClassArrayTraits
 			public static function generate_classarray()
 			public function _toArray()
@@ -94,7 +175,7 @@
 			unset($array['stancost']);
 			return $array;
 		}
-		
+
 		/* =============================================================
 			CRUD FUNCTIONS
 		============================================================ */
@@ -106,7 +187,7 @@
 		public function create($debug = false) {
 			return insert_cartdetail($this->sessionid, $this, $debug);
 		}
-		
+
 		/**
 		 * Reads the Cart Detail from the Database
 		 * @param  string  $sessionID Session ID
@@ -117,6 +198,7 @@
 		public static function load($sessionID, $linenbr, $debug = false) {
 			return get_cartdetail($sessionID, $linenbr, $debug);
 		}
+
 		/**
 		 * CartDetail submits changes to the record in the Database
 		 * @param  bool   $debug Whether SQL executes or not
@@ -125,6 +207,7 @@
 		public function update($debug = false) {
 			return update_cartdetail($this->sessionid, $this, $debug);
 		}
+
 		/**
 		 * Checks if changes were made to the Cart Detail
 		 * @return bool If this has any differences from original record
@@ -132,7 +215,7 @@
 		public function has_changes() {
 			$properties = array_keys(get_object_vars($this));
 			$detail = self::load($this->sessionid, $this->linenbr, false);
-			
+
 			foreach ($properties as $property) {
 				if ($this->$property != $detail->$property) {
 					return true;

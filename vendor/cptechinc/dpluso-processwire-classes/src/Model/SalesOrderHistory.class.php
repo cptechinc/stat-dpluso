fdc62d678d78bd9d488db0dd08267ab3fde5eecf
Paul Gomez <pauldro952@outlook.com>
2018-06-27 09:24:45 -0500

    Changed static function for getting custid
    (cherry picked from commit cptechinc/soft-dpluso@3ac46be839b88b982df163bc9568d9827a2e90cc)

--- vendor/cptechinc/dpluso-processwire-classes/src/Model/SalesOrderHistory.class.php
+++ vendor/cptechinc/dpluso-processwire-classes/src/Model/SalesOrderHistory.class.php
@@ -96,7 +96,11 @@
 			return is_ordersaleshistory($ordn, $debug);
 		}
 		
-		public static function get_custid($ordn, $debug = false) {
-			return get_custidfromsaleshistory($ordn, $debug);
+		public static function get_custid($ordn = '', $debug = false) {
+			if (isset($this)) {
+				return $this->orderno;
+			} else {
+				return get_custidfromsaleshistory($ordn, $debug);
+			}
 		}
 	}

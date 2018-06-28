efceb71b1624162316d4af4139e02ed0ad7912c8
bbullemer <barbara.bullemer@gmail.com>
2018-06-28 10:20:02 -0500

    Fix for User day tasks
    - fix to show day tasks on the user action panel
    
    (cherry picked from commit cptechinc/soft-dpluso@9df3d81e79789ddb0ef43b002891e692b170e5c9)

--- vendor/cptechinc/dpluso-processwire-classes/src/OrderPanel.class.php
+++ vendor/cptechinc/dpluso-processwire-classes/src/OrderPanel.class.php
@@ -1,88 +1,88 @@
-<?php 
+<?php
 	/**
 	 * Blueprint class for dealing with lists of orders and their display
 	 */
 	abstract class OrderPanel extends OrderDisplay {
 		use AttributeParser;
-		
+
 		/**
 		 * ID of HTML element to focus on after ajax load
 		 * @var string e.g. #orderpanel
 		 */
 		public $focus;
-		
+
 		/**
 		 * ID of HTML element to load into after ajax load
 		 * @var string e.g. #orderpanel
 		 */
 		public $loadinto;
-		
+
 		/**
 		 * String of data attributes
 		 * @var string e.g. data-loadinto='$this->loadinto' data-focus='$this->focus'
 		 */
 		public $ajaxdata;
-		
+
 		/**
 		 * Segment of URL to place the pagination segment
 		 * @var string
 		 */
 		public $paginationinsertafter;
-		
+
 		/**
 		 * Boolean to decide if this has been loaded through ajax
 		 * @var bool
 		 */
 		public $throughajax;
-		
+
 		/**
 		 * Whether or not the panel div shows opened or collapse
 		 * @var string
 		 */
 		public $collapse = 'collapse';
-		
+
 		/**
 		 * Object to sort the columns
 		 * @var TablePageSorter
 		 */
 		public $tablesorter; // Will be instatnce of TablePageSorter
-		
+
 		/**
 		 * Page Number
 		 * @var int
 		 */
 		public $pagenbr;
-		
+
 		/**
-		 * Which Order Number is the active Order 
+		 * Which Order Number is the active Order
 		 * @var string
 		 */
 		public $activeID = false;
-		
+
 		/**
 		 * Number of Orders
 		 * @var int
 		 */
 		public $count;
-		
+
 		/**
 		 * Array of filters that will apply to the orders
 		 * @var array
 		 */
 		public $filters = false; // Will be instance of array
-		
+
 		/**
 		 * Array of key->array of filterable columns
 		 * @var array
 		 */
 		public $filterable;
-		
+
 		/**
 		 * Panel Type
 		 * @var string
 		 */
 		public $paneltype;
-		
+
 		/**
 		 * Constructor
 		 * @param string  $sessionID  Session Identifier
@@ -97,21 +97,21 @@
 			$this->loadinto = $this->focus = $loadinto;
 			$this->ajaxdata = "data-loadinto='$this->loadinto' data-focus='$this->focus'";
 			$this->tablesorter = new TablePageSorter($this->pageurl->query->get('orderby'));
-			
+
 			if ($ajax) {
 				$this->collapse = '';
 			} else {
 				$this->collapse = 'collapse';
 			}
 		}
-		
+
 		/**
 		 * Setup the Page URL then add the necessary components in the path and querystring
 		 * @return void
 		 * @uses parent::setup_pageurl()
 		 */
 		abstract public function setup_pageurl();
-		
+
 		/* =============================================================
 			Class Functions
 		============================================================ */
@@ -122,7 +122,7 @@
 		public function generate_pagenumberdescription() {
 			return ($this->pagenbr > 1) ? "Page $this->pagenbr" : '';
 		}
-		
+
 		/**
 		 * Returns HTML for a popover that has the shipto address
 		 * @param  Order  $order Gets the Address info from $order
@@ -137,7 +137,7 @@
 			$attr .= "|data-placement=top|data-trigger=focus|data-html=true|title=Ship-To Address|data-content=$address";
 			return $bootstrap->openandclose('a', $attr, '<b>?</b>');
 		}
-		
+
 		/* =============================================================
 			OrderPanelInterface Functions
 		============================================================ */
@@ -152,7 +152,7 @@
 			$ajaxdata = $this->generate_ajaxdataforcontento();
 			return $bootstrap->openandclose('a', "href=$href|class=generate-load-link btn btn-warning btn-block|$ajaxdata", "Clear Search $icon");
 		}
-		
+
 		/**
 		 * Returns HTML link for clearing the sort
 		 * @return [HTML Link
@@ -163,7 +163,7 @@
 			$ajaxdata = $this->generate_ajaxdataforcontento();
 			return $bootstrap->openandclose('a', "href=$href|class=btn btn-warning btn-sm load-link|$ajaxdata", '(Clear Sort)');
 		}
-		
+
 		/**
 		 * Returns URL with the sort parameters removed
 		 * @return string URL to load
@@ -173,7 +173,7 @@
 			$url->query->remove("orderby");
 			return $url->getUrl();
 		}
-		
+
 		/**
 		 * Returns HTML link to load the Orders/Quotes
 		 * @return string HTML link
@@ -184,7 +184,7 @@
 			$ajaxdata = $this->generate_ajaxdataforcontento();
 			return $bootstrap->openandclose('a', "href=$href|class=generate-load-link|$ajaxdata", "Load Orders");
 		}
-		
+
 		/**
 		 * Returns the sortby column URL
 		 * @param  string $column column to sortby
@@ -195,8 +195,8 @@
 			$url->query->set("orderby", "$column-".$this->tablesorter->generate_columnsortingrule($column));
 			return $url->getUrl();
 		}
-		
-		
+
+
 		/**
 		 * Looks through the $input->get for properties that have the same name
 		 * as filterable properties, then we populate $this->filter with the key and value
@@ -223,8 +223,8 @@
 				}
 			}
 		}
-		
-		
+
+
 		/**
 		 * Looks through the $input->get for properties that have the same name
 		 * as filterable properties, then we populate $this->filter with the key and value
@@ -251,7 +251,7 @@
 				}
 			}
 		}
-		
+
 		/**
 		 * Grab the value of the filter at index
 		 * Goes through the $this->filters array, looks at index $filtername
@@ -267,7 +267,7 @@
 			}
 			return '';
 		}
-		
+
 		/**
 		 * Checks if $this->filters has value of $value
 		 * @param  string $key        string
@@ -278,7 +278,7 @@
 			if (empty($this->filters)) return false;
 			return (isset($this->filters[$key])) ? in_array($value, $this->filters[$key]) : false;
 		}
-		
+
 		/**
 		 * Returns a descrption of the filters being applied to the orderpanel
 		 * @return string Description of the filters
@@ -286,13 +286,13 @@
 		public function generate_filterdescription() {
 			if (empty($this->filters)) return '';
 			$desc = 'Searching '.$this->generate_paneltypedescription().' with';
-			
+
 			foreach ($this->filters as $filter => $value) {
 				$desc .= " " . QueryBuilder::generate_filterdescription($filter, $value, $this->filterable);
 			}
 			return $desc;
 		}
-		
+
 		/**
 		 * Returns the orders description e.g. sales order
 		 * @return string Panel Type Description

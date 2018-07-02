6abf714831668541321fa5271742b1ed03d541c7
Paul Gomez <pauldro952@outlook.com>
2018-07-02 08:39:47 -0500

    Sales History Panel logic to see if Qty Per Case  Module is installed
    (cherry picked from commit cptechinc/soft-dpluso@2c24e365abd85b549a81186b6e8024be84973a7e)

--- site/templates/content/dashboard/sales-history/sales-history-panel.php
+++ site/templates/content/dashboard/sales-history/sales-history-panel.php
@@ -36,8 +36,14 @@
 			</div>
 		  </div>
 		<div class="table-responsive">
-			<?php include $config->paths->content.'dashboard/sales-history/table.php'; ?>
-			<?= $paginator; ?>
+			<?php
+				if ($modules->isInstalled('QtyPerCase')) {
+					include $config->paths->siteModules.'QtyPerCase/content/customer/sales-history/table.php';
+				} else {
+					include $config->paths->content.'customer/cust-page/sales-history/table.php';
+				}
+				echo $paginator;
+			?>
 		</div>
 	 </div>
 </div>
